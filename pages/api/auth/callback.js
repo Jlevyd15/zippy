779c524fbab47a90c01f8dec414016846aec1453
Jeremy Levy <jlevy@turo.com>
2023-08-23 16:02:11 +0000

    feat(yarn) add yarn.lock

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,9 +1,8 @@
 // This api sends an SMS
 export default async function callback(req, res) {
-  console.log("Success from Tesla! ", req.body);
-
+  const requestData = req.body;
   // grab the code
-  const { code } = req.body;
+  const { code, test } = req.body;
 
   const dataToSend = {
     grant_type: "authorization_code",
@@ -27,25 +26,30 @@ export default async function callback(req, res) {
   if (test) {
     console.log(`in test POST, ${JSON.stringify(requestData)}`);
     res.json({ message: "Received callback", data: requestData });
-  }
+  } else {
+    try {
+      const response = await fetch(
+        "https://auth.tesla.com/oauth2/v3/token",
+        requestOptions
+      );
+      console.log("Starting code exchange");
+      const json = await response.json();
 
-  try {
-    const response = await fetch(
-      "https://auth.tesla.com/oauth2/v3/token",
-      requestOptions
-    );
-    console.log("Starting code exchange");
-    const json = await response.json();
-    const { access_token } = json.body;
-    res.json({ message: "Received token", data: { token: access_token } });
-  } catch (err) {
-    console.error("Error in callback from Tesla", err);
-    res.json({
-      message: "Error in callback from Tesla",
-      code: 500,
-      stack: err,
-    });
+      const { access_token } = json;
+      console.log(json);
+      if (!access_token) {
+        throw new Error(JSON.stringify(json));
+      }
+      res.json({
+        message: "Received token",
+        data: { token: access_token },
+      });
+    } catch (err) {
+      res.json({
+        message: "Error in callback from Tesla",
+        code: 500,
+        stack: `${err}`,
+      });
+    }
   }
-
-  res.end();
 }
